a5eddb5c8724da6c92b43957c2eea3578136f6be
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2020-11-04 23:21:31 +0900

    Made timer

--- games/carrot/main.js
+++ games/carrot/main.js
@@ -6,22 +6,58 @@ const carrot = document.querySelector('.carrots');
 
 // timer
 const pause = document.querySelector('.pause__box');
-const time = document.querySelector('.timer');
-const counter = document.querySelector('.count');
+const timeElem = document.querySelector('.timer');
+const counterElem = document.querySelector('.count');
 
-const myTest = new Array('carrot','bug'); // 뉴어레이에 
+// result
+const homeBtn = document.querySelectorAll('.home__contact')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelectorAll('.modal__overlay')
+const closeBtn = document.querySelectorAll('.closeBtn')
 
-function randomItem(a){
-    
-  return a[Math.floor(Math.random()*a.length)]
+const openModal = () => {
+    document.querySelector('.modal').classList.add('hidden')
 }
-
-
-let 그릇 = randomItem(myTest);
-let 후보군 = Array(10).fill().map(function (그릇, 인덱스) { //맵으로 string 매칭 가능한지
-    return 그릇;
+const closeModal = (event) => {
+    event.currentTarget.parentNode.classList.remove('hidden')
+}
+overlay.forEach((event) => {
+    event.addEventListener('click', closeModal)
 })
+closeBtn.forEach((event) => {
+    event.addEventListener('click', closeModal)
+})
+bug.addEventListener('click', openModal);
 
-console.log(후보군)
-console.log(randomItem(myTest))
-console.log(randomItem(myTest))
+
+// timer
+let count = 3;
+let counter =setInterval(timer, 1000); 
+function timer(){
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+    //  modal.classList.add('hidden')
+    // return
+    count = 0
+    } 
+
+ timeElem.innerHTML= " 00:" + count ; // watch for spelling
+}
+    
+timer();
+    
+
+//carrot
+const countDown = () => {
+    document.querySelector('.modal').classList.add('hidden')
+}
+carrot.addEventListener('click', countDown);
+
+
+
+
+
+
+
